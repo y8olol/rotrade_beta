7cb656fe65de9996a0fff9da03f1205bfea91292
pieter <247892592+y8olol@users.noreply.github.com>
2026-01-21 15:39:31 -0500

    Add files via upload

---
+++ background/cache.js
@@ -0,0 +1,26 @@
+let rolimonsCache = {
+    data: null,
+    timestamp: 0,
+    promise: null,
+    duration: 60000
+};
+
+let commonOwnersCache = {
+    map: new Map(), 
+    duration: 60000
+};
+
+let thumbnailCache = {
+    map: new Map(),
+    duration: 300000
+};
+
+let inventoryCache = {
+    map: new Map(),
+    duration: 60000
+};
+
+let playerAssetsCache = {
+    map: new Map(),
+    duration: 60000
+};
